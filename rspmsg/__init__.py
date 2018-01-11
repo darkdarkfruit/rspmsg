6db9ce564c30f50bec1ca25eac7ecb67a80824d9
darkdarkfruit <darkdarkfruit@gmail.com>
2018-01-11 17:42:46 +0800

    v0.6.1

--- rspmsg/__init__.py
+++ rspmsg/__init__.py
@@ -20,7 +20,7 @@ from .rspmsg import Message, make_successful_message, make_failed_message, s, f
 
 __author__ = 'darkdarkfruit'
 
-VERSION_TUPLE = (0, 6, 0)
+VERSION_TUPLE = (0, 6, 1)
 VERSION_TUPLE_IN_STR = [str(i) for i in VERSION_TUPLE]
 
 
